6db3a20ebac41c339f540f7109c4297a0884d863
Just-iiiiiiiiiiiiiin <wyslijtokajaki@gmail.com>
2022-03-25 09:40:59 +0100

    a

--- js/events2.js
+++ js/events2.js
@@ -32,3 +32,16 @@ $(function(){
     });
 
 });
+
+
+$(function (){
+    $('#wysun').on('click', function (){
+        $('#pierwszy').toggle().slideDown(1000);
+    });
+    $('#wysun2').on('click', function (){
+        $('#drugi').toggle().slideUp(2000);
+    });
+    $('#wysun3').on('click', function (){
+        $('#trzeci').toggle().slideToggle('slow');
+    });
+});
