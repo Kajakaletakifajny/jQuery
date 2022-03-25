9ab5e285f26fdf4962642d86decb13ad6abeb29f
Just-iiiiiiiiiiiiiin <wyslijtokajaki@gmail.com>
2022-03-25 10:21:29 +0100

    a

--- js/events2.js
+++ js/events2.js
@@ -33,7 +33,7 @@ $(function(){
 
 });
 
-
+//kwadraty zwijające się
 $(function (){
     $('#wysun').on('click', function (){
         $('#pierwszy').toggle().slideDown(1000);
@@ -42,6 +42,35 @@ $(function (){
         $('#drugi').toggle().slideUp(2000);
     });
     $('#wysun3').on('click', function (){
-        $('#trzeci').toggle().slideToggle('slow');
+        $('#trzeci').slideToggle('slow');
+    });
+});
+
+//kulki
+$(function (){
+    $('#start1').on('click', function (){
+        $('#pierwszy2').animate({'left': '+=300px'}, 'slow');
+        $('#pierwszy2').animate({'top': '+=50px'}, 'slow');
+        $('#pierwszy2').animate({'left': '+=700px'}, 600);
+    });
+
+    $('#start2').on('click', function (){
+        $('#drugi2').animate({'left': '+=300px'}, 'slow');
+        $('#drugi2').animate({'top': '+=50px'}, 'slow');
+        $('#drugi2').animate({'left': '+=700px'}, 800);
+    });
+
+    $('#start3').on('click', function (){
+        $('#start1 , #start2').click();
+    });
+
+    $('#reset').on('click', function (){
+        $('#log').text('');
+        $('#pierwszy2 , #drugi2').css({left: '' , top: ''});
+    });
+
+    $('#stop').on('click', function () {
+        $('.kulka').clearQueue();
+        $('.kulka').stop();
     });
 });
